35bb9d2f2b795a8309e7775791427f6b91fe46c3
Mayuthean <bunthorngmayuthean@gmail.com>
2019-03-11 13:54:04 +0700

    already

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,16 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+  def new
+    @movie = Movie.new
+  end
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movie_path(@movie)
+  end
+  private
+    def movie_params
+        params.require(:movie).permit(:title, :autor, :released, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get "movies" => "movies#index"
+  get "movies" => "movies#index", :as => "movies"
+  get "movies/new" => "movies#new"
+  post "movies" => "movies#create"
   get "movies/:id" => "movies#show", :as => "movie"
 end
