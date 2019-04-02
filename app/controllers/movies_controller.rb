1ffdaa2142c20b6a6dd647bb033415b0e2e4eb54
Mayuthean <bunthorngmayuthean@gmail.com>
2019-04-02 11:18:03 +0700

    I have fix something

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,8 +13,23 @@ class MoviesController < ApplicationController
     @movie.save
     redirect_to movie_path(@movie)
   end
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
   private
     def movie_params
         params.require(:movie).permit(:title, :autor, :released, :description)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get "movies" => "movies#index", :as => "movies"
-  get "movies/new" => "movies#new"
-  post "movies" => "movies#create"
-  get "movies/:id" => "movies#show", :as => "movie"
+  # get "movies" => "movies#index", :as => "movies"
+  # get "movies/new" => "movies#new"
+  # post "movies" => "movies#create", :as => "create"
+  # get "movies/:id" => "movies#show", :as => "movie"
+  # get "movies/:id/edit" => "movies#edit", :as => "edit_movie"
+  # patch "movies/:id" => "movies#update"
+  # put "movies/:id" => "movies#update"
+  # delete "movies/:id" => "movies#destroy"
+  resources:movies
 end
